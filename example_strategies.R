48838027df6cf26e8e396da1089f0e5aa362c09d
TANKERS888 <32858582+TANKERS888@users.noreply.github.com>
2017-11-11 14:54:22 +0000

    Update example_strategies.R

--- example_strategies.R
+++ example_strategies.R
@@ -1,31 +1,13 @@
-example_strategies <- c("fixed", 
-                        "big_spender",
-                        "bankrupt", 
-                        "copycat", 
-                        "random", 
-                        "rsi_contrarian", 
-                        "bbands_trend_following",
-                        "bbands_contrarian",
-                        "bbands_holding_period",
-                        "simple_limit",
+example_strategies <- c(
                         "macd",
-                        "team5"
+                        "kdj"
                         )
 
-example_params <- list(
-                    "team5" = list(series=1:10,volwindow=60,slow=21,fast=15,signal=10,trendSlowLine=50,
-                    lookback=50,wprn=6),
-                    "macd"=list(series=1:10,slow=26,fast=12,signal=9,wprn=10),
-                    "fixed"=list(sizes=rep(1,10)),
-                    "big_spender"=list(sizes=rep(1,10)),
-                    "bankrupt"=list(leverage=40000000),
-                    "copycat"=NULL,
-                    "random"=list(maxLots=100),
-                    "rsi_contrarian"=list(lookback=10,threshold=25,series=1:5),
-                    "bbands_contrarian"=list(lookback=20,sdParam=1.5,series=1:4,posSizes=rep(1,10)),
-                    "bbands_trend_following"=list(lookback=50,sdParam=1.5,series=c(1,3,5,7,8,9),posSizes=rep(1,10)),
-                    "bbands_holding_period"=list(lookback=50,sdParam=1.5,series=c(1,3),posSizes=rep(1,10),holdPeriod=6),
-                    "simple_limit"=list(spreadPercentage=0.001,inventoryLimits=rep(10,10),series=9:10))
+example_params<-list(
+                    "kdj"=list(series=1:10,nFastK=12,nFastD=9,nSlowD=26,lookback=50,size=1000),
+                    "macd"=list(series=1:10,slow=26,fast=12,signal=9,size=400000,profitTarget=0.15                   
+                    )
+                      
 
 load_strategy <- function(strategy) {
 
