2892f5a0647ed4011fb196964167977aff4a3a53
TANKERS888 <32858582+TANKERS888@users.noreply.github.com>
2017-11-11 14:50:17 +0000

    Update kdj.R

--- kdj.R
+++ kdj.R
@@ -1,61 +1,60 @@
-#kdj
+##########
+#  KDJ   #
+##########
+maxRows <- 10000 
 
-maxRows <- 3300 # used to initialize a matrix to store closing prices
+###################### getOrders ############################
 
-getOrders <- function(store, newRowList, currentPos, params) {
+getOrders <- function(store,newRowList,currentPos,info,params) {
   
-  allzero  <- rep(0,length(newRowList)) # used for initializing vectors
+  allzero  <- rep(0,length(newRowList)) 
   
-  if (is.null(store)) store <- initStore(newRowList,params$series)
-  store <- updateStore(store, newRowList, params$series)
+  if (is.null(store)) {
+    store <- initStore(newRowList,params$series)
+  }
+  else 
+    store <- updateStore(store, newRowList,params$series)
+
+    pos <- allzero
   
-  marketOrders <- -currentPos; pos <- allzero
   
   if ((store$iter>params$lookback-1)) { 
     
     for (i in 1:length(params$series)) {
       
       startIndex <- store$iter - params$lookback 
+      
       cl <- newRowList[[params$series[i]]]$Close
       
-      kdj <- stoch(cbind(store$hg[startIndex:store$iter,i],store$lw[startIndex:store$iter,i],
+      
+      kdj <- stoch(cbind(store$hi[startIndex:store$iter,i],
+                         store$lo[startIndex:store$iter,i],
                          store$cl[startIndex:store$iter,i]),
-                   nFastK = params$nFastK, nFastD = params$nFastD, nSlowD = params$nSlowD, 
-                   maType = list(list(SMA),list(EMA,wilder=TRUE),list(SMA)), bounded= TRUE,smooth=1)*100
+                   nFastK = params$nFastK, 
+                   nFastD = params$nFastD, 
+                   nSlowD = params$nSlowD, 
+                   maType = list(list(SMA),list(EMA,wilder=TRUE),list(SMA)), 
+                   bounded= TRUE,
+                   smooth=1)*100
+      
       KDJ <- last(kdj)
+    
+      J <- 3*as.numeric(KDJ[["fastD"]]) - 2*as.numeric(KDJ[["fastK"]])  #J line
+      D <- as.numeric(KDJ[["fastD"]])  #D line
+      K <- as.numeric(KDJ[["fastK"]]) #K line
       
-      
-      fastJ <- 3*as.numeric(KDJ[["fastD"]]) - 2*as.numeric(KDJ[["fastK"]])     
-      if (fastJ < 0) {
-        pos[params$series[i]] <- -params$posSizes[params$series[i]]
+      if (K <20 && D<30 && J<0) {
+        pos[params$series[i]] <- -(params$size/last(as.numeric(cl)))  
       }
-      else if (fastJ > 100){
-        pos[params$series[i]] <- params$posSizes[params$series[i]]
-      }
-      else if (as.numeric(KDJ[["fastK"]]) < 20){ # <50
-       # if (as.numeric(KDJ[["fastK"]]) < 20 ){
-          pos[params$series[i]] <- -params$posSizes[params$series[i]]
-        }
-        #else if(as.numeric(KDJ[["fastD"]])==as.numeric(KDJ[["fastK"]]) ){
-        # pos[params$series[i]] <- -params$posSizes[params$series[i]]
-        #}
-      #} 
-      else if (as.numeric(KDJ[["fastK"]]) > 80){  # >=50
-       # if (as.numeric(KDJ[["fastK"]]) >80 ){
-          pos[params$series[i]] <- params$posSizes[params$series[i]]
-        }
-        else if(as.numeric(KDJ[["fastD"]])==as.numeric(KDJ[["fastK"]]) ){
-          pos[params$series[i]] <- params$posSizes[params$series[i]]
-       # }
-      }
+      else if(K >80 && D >70 && J>90)
+        pos[params$series[i]] <- (params$size/last(as.numeric(cl)))
     }
   }
   
-  marketOrders <- marketOrders + pos
+  marketOrders <-  pos
   
   
   # exit positions from yesterday
-  
   return(list(store=store,marketOrders=marketOrders,
               limitOrders1=allzero,
               limitPrices1=allzero,
@@ -64,56 +63,67 @@ getOrders <- function(store, newRowList, currentPos, params) {
 }
 
 
-####################################################################################
-# All the subsequent functions were designed to simplify and 
-# improve the readaility of getNewPos(); 
-#
-# While not required, this type of function-based approach is advisable 
-# and these functions will likely have analogues in your strategies
-####################################################################################
+
+
+######## Managing the store (high, low, close) ##############
+
+
+
+#close INITIAL
 initClStore  <- function(newRowList,series) {
-  clStore <- matrix(0,nrow=maxRows,ncol=length(series))
+  clStore <- matrix(0,
+                    nrow=maxRows,
+                    ncol=length(series))
   return(clStore)
 }
+#high INITIAL
+initHiStore  <- function(newRowList,series) {
+  hiStore <- matrix(0,
+                    nrow=maxRows,
+                    ncol=length(series))
+  return(hiStore)
+}
+#low INITIAL
+initLoStore  <- function(newRowList,series) {
+  loStore <- matrix(0,
+                    nrow=maxRows,
+                    ncol=length(series))
+  return(loStore)
+}
+#ALL INITIAL
+initStore <- function(newRowList,series) {
+  return(list(iter=0,
+              cl=initClStore(newRowList,series),
+              hi=initHiStore(newRowList,series),
+              lo=initLoStore(newRowList,series)))
+}
 
+#close UPDATE
 updateClStore <- function(clStore, newRowList, series, iter) {
   for (i in 1:length(series))
     clStore[iter,i] <- as.numeric(newRowList[[series[i]]]$Close)
   return(clStore)
 }
 
-initStore <- function(newRowList,series) {
-  return(list(iter=0,cl=initClStore(newRowList,series),
-              hg=initHgStore(newRowList,series),
-              lw=initLwStore(newRowList,series)))
+#high UPDATE
+updateHiStore <- function(hiStore, newRowList, series, iter) {
+  for (i in 1:length(series))
+    hiStore[iter,i] <- as.numeric(newRowList[[series[i]]]$High)
+  return(hiStore)
 }
 
+#low UPDATE
+updateLoStore <- function(loStore, newRowList, series, iter) {
+  for (i in 1:length(series))
+    loStore[iter,i] <- as.numeric(newRowList[[series[i]]]$Low)
+  return(loStore)
+}
+
+#ALL UPDATE
 updateStore <- function(store, newRowList, series) {
   store$iter <- store$iter + 1
-  store$cl <- updateClStore(store$cl,newRowList,series,store$iter)
-  store$hg <- updateClStore(store$hg,newRowList,series,store$iter) 
-  store$lw <- updateClStore(store$lw,newRowList,series,store$iter) 
+  store$cl <- updateClStore(store$cl,newRowList,series,store$iter) 
+  store$hi <- updateHiStore(store$hi,newRowList,series,store$iter) 
+  store$lo <- updateLoStore(store$lo,newRowList,series,store$iter) 
   return(store)
-}
-
-initHgStore  <- function(newRowList,series) {
-  hgStore <- matrix(0,nrow=maxRows,ncol=length(series))
-  return(hgStore)
-}
-
-initLwStore  <- function(newRowList,series) {
-  lwStore <- matrix(0,nrow=maxRows,ncol=length(series))
-  return(lwStore)
-}
-
-updateHgStore <- function(hgStore, newRowList, series, iter) {
-  for (i in 1:length(series))
-    hgStore[iter,i] <- as.numeric(newRowList[[series[i]]]$High)
-  return(hgStore)
-}
-
-updateLwStore <- function(lwStore, newRowList, series, iter) {
-  for (i in 1:length(series))
-    lwStore[iter,i] <- as.numeric(newRowList[[series[i]]]$Low)
-  return(lwStore)
 }
